79dfc98dda25de8183898772e2f9b4a76735c81e
vzer6 <3513394098@qq.com>
2023-05-17 21:15:24 +0800

    .
    好了

--- src/models/Block.js
+++ src/models/Block.js
@@ -1,9 +1,24 @@
-class Block {
-  // 1. 完成构造函数及其参数
-  /* 构造函数需要包含
+import sha256 from 'crypto-js/sha256.js'
 
-  */
-  constructor() {}
+export const DIFFICULTY = 2
+
+class Block {
+    // 1. 完成构造函数及其参数
+
+    constructor(blockchain, previousHash, index, hash) {
+        this.blockchain = blockchain;
+        this.previousHash = previousHash
+        this.height = index
+        this.hash = hash
+    }
+        /**
+         * 
+         * @returns 该区块的前一个区块
+         */
+    getPreviousBlock() {
+        return this.blockchain.blocks[this.previousHash]
+    }
+
 }
 
 export default Block
