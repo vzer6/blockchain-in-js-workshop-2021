e08f435e82f89b703d22e07b883cca632de9bd84
vzer6 <3513394098@qq.com>
2023-05-17 22:08:53 +0800

    ok
    ok

--- src/models/Block.js
+++ src/models/Block.js
@@ -1,22 +1,34 @@
 import sha256 from 'crypto-js/sha256.js'
-
-export const DIFFICULTY = 2
-
 class Block {
     // 1. 完成构造函数及其参数
 
+    /* 构造函数需要包含
+        所属的区块链
+        前一个区块的Hash
+        索引
+        当前区块的Hash
+    */
     constructor(blockchain, previousHash, index, hash) {
         this.blockchain = blockchain;
         this.previousHash = previousHash
-        this.height = index
+        this.index = index
         this.hash = hash
     }
-        /**
-         * 
-         * @returns 该区块的前一个区块
-         */
     getPreviousBlock() {
-        return this.blockchain.blocks[this.previousHash]
+            return this.blockchain.blocks[this.previousHash]
+        }
+        /**
+         * 计算区块Hash的方法
+         * @returns 区块的Hash值
+         */
+    calculateHash() {
+        return sha256(
+            this.previousHash +
+            this.height +
+            this.data +
+            this.timestamp +
+            this.blockchain.name,
+        ).toString()
     }
 
 }
